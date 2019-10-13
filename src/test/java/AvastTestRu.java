e0337689e9157d4d100b5d83ed766e6269b1d964
BetkaZ <56506119+BetkaZ@users.noreply.github.com>
2019-10-13 14:33:57 +0200

    Test Avast

---
+++ src/test/java/AvastTestRu.java
@@ -0,0 +1,46 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class AvastTestRu {
+
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void setUp() {
+		Path path = FileSystems.getDefault().getPath("src/main/resources/geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", path.toString());
+		driver = new FirefoxDriver();
+
+	}
+	@Test
+	public void testAvastRu() {
+		driver.get("https://www.avast.com/ru-ru/lp-business-aff-secureline-45");
+		WebElement cookies = driver.findElement(By.className("js-close"));
+		if (cookies.isDisplayed())
+			cookies.click();
+		// driver.findElement(By.id("1year5pcs")).click();
+		String price = driver.findElement(By.className("smb-abox_price_discounted")).getText();
+		driver.findElement(By.className("smb-abox_btn-box-wrapper")).findElement(By.tagName("a")).click();
+		String cartPrice = driver.findElement(By.className("av_lineItem-listPrice")).getText();
+		Assert.assertEquals(price, cartPrice);
+
+
+	}
+	@AfterClass
+	public static void cleanUp() {
+		if (driver != null) {
+			driver.close();
+
+		}
+	}
+
+}
